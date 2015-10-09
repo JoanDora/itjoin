dc2093606e9b7aed23109ec40951ae7d3a592242
solr <liuyao64@sina.com>
2015-10-09 08:45:41 +0800

    ss

---
+++ src/main/java/com/itjoin/main/MainController.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
+ *  
+ * This code is the confidential and proprietary information of   
+ * Hzins. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Hzins,http://www.hzins.com.
+ *  
+ */   
+package com.itjoin.main; 
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>
+ * 
+ *
+ *
+ * </p>
+ * @author	hz1412998 
+ * @date	2015年10月9日 上午8:39:54
+ * @version      
+ */
+@Controller
+public class MainController {
+    
+    private static final String PAGE="pages";
+
+    @RequestMapping("/")
+   public String main(HttpServletRequest request){
+	return PAGE+"/index";
+    }
+}
+ 
